a97111205c5925a329a4b339a67ecf04ab812869
eunbi99 <>
2023-12-28 18:42:38 +0900

    java의 Synchronized 사용과 @Transactional을 동시에 사용하면 발생하는 문제점 (레이스컨디션)

--- stock/src/main/java/com/example/stock/servie/StockService.java
+++ stock/src/main/java/com/example/stock/servie/StockService.java
@@ -2,6 +2,7 @@ package com.example.stock.servie;
 
 import com.example.stock.domain.Stock;
 import com.example.stock.repository.StockRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,10 +12,8 @@ public class StockService {
 
     private final StockRepository stockRepository;
 
-    public void decrease(Long id, Long quantity) {
-        // stock조회
-        // 재고 감소시킨뒤
-        // 갱신된 값을 저장한다.
+    @Transactional
+    public synchronized void decrease(Long id, Long quantity) {
         Stock stock = stockRepository.findById(id).orElseThrow();
         stock.decrease(quantity);
 
